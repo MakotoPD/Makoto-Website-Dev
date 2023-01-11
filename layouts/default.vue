e263ad5d403c85b31365302c95f6252ed736bda3
MakotoPL <patrydab4@gmail.com>
2023-01-11 16:05:47 +0100

    setup

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="bg-gray-900 w-screen h-screen">
+		<div class="w-full flex flex-col justify-center">
+			<div class="relative w-full flex justify-between p-2 border-b border-gray-500">
+				<div>
+					<NuxtLink to="/">
+						<img src="/logo.png" alt="logo.png" class="h-8">
+					</NuxtLink>
+				</div>
+				<div class="flex flex-nowrap space-x-4 items-center text-gray-400 pr-4">
+					<NuxtLink to="/">
+						Home
+					</NuxtLink>
+					<NuxtLink to="/Portfolio">
+						Portfolio
+					</NuxtLink>
+					<NuxtLink to="/Contact">
+						Contact
+					</NuxtLink>
+					<NuxtLink to="/MyStack">
+						My Stack
+					</NuxtLink>
+					<NuxtLink to="/Blog">
+						Contact
+					</NuxtLink>
+					<NuxtLink to="/Login">
+						Login
+					</NuxtLink>
+				</div>
+				<div class="absolute right-4 -bottom-8 h-8 w-20 flex justify-center items-center rounded-b-md text-white font-semibold bg-blue-400/90">
+					Beta
+				</div>
+			</div>
+			<img src="/x.png" alt="background" class="h-10">
+		</div>
+
+		<slot />
+	</div>
+
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,51 @@
+export default defineNuxtConfig({
+	app:{
+		head: {
+			title: 'Makoto - Website developer',
+			htmlAttrs: {
+			  lang: 'en',
+			},
+			meta: [
+			  { charset: 'utf-8' },
+			  { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			  { hid: 'description', name: 'description', content: '' },
+			  { name: 'format-detection', content: 'telephone=no' },
+			  { name: 'keywords', content: ''}
+			],
+			link: [
+			  { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			  { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+			  { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+			  { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:display=swap' },
+			  { rel: 'stylesheet', href: 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' } 
+			],
+		},
+
+		pageTransition: { name: 'page', mode: 'out-in' },
+		layoutTransition: { name: 'layout', mode: 'out-in' },
+
+	},
+
+	modules: [
+		'@nuxtjs/tailwindcss',  	//css framework
+		'~/modules/sitemap',		//sitemap creator
+		'@unlighthouse/nuxt',		//lighthouse buided in page, port:5678
+	],
+
+	builder: 'vite',			//webpack alternative for nuxtjs/vuejs
+	
+	sitemap: {
+		hostname: 'https://makoto.click',
+	},
+
+
+	unlighthouse: {
+		scanner: {
+		  device: 'desktop', // simulate a desktop device
+		}
+	},
+	
+	css: [
+		'@/assets/css/global.css',
+	]
+})
