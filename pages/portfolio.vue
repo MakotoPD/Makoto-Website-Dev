e450ed31835ff48106fea477e76493fbe423da1e
MakotoPL <patrydab4@gmail.com>
2023-05-22 21:01:45 +0200

    add portfolio

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -4,9 +4,20 @@
 			<Title>Makoto - {{ $t('page.portfolio.title') }}</Title>
 		</Head>
 
+		<div class="container mx-auto px-4 md:px-36">
+			<div class="items-center pt-24 mb-24 md:mb-44">
+				<h1 class="text-white text-4xl">{{ $t('page.portfolio.title') }}</h1>
+				<h2 class="text-primary text-3xl font-light">{{ $t('page.portfolio.subtitle') }}</h2>
+			</div>
+		</div>
 		
-		<div class="flex justify-center items-center">
-			portfolio
+
+		<div class="container mx-auto mb-12">
+			<div class="grid grid-cols-2 md:grid-cols-3 place-items-center">
+				<div v-for="companies in imgs.logos"  class="m-6 xl:h-64 xl:w-64 overflow-hidden rounded-lg">
+					<nuxt-img preload  loading="lazy" quality="30" :src="companies.src" :alt="companies.alt"  />
+				</div>
+			</div>
 		</div>
 	</div>
 
@@ -16,4 +27,6 @@
 useHead({
 	title: 'Portfolio - Makoto',
 })
+
+import imgs from '~/static/json/logos.json'
 </script>
