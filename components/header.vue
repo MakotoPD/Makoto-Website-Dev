7ba9c697edd6cc37f127aab580cddebc01e4e813
MakotoPL <patrydab4@gmail.com>
2023-06-14 14:06:17 +0200

    Update header.vue

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flex flex-col justify-center items-center pt-14 pb-0 md:py-14">
-		<h1 data-aos="zoom-in" class="text-5xl md:text-7xl font-semibold text-white">{{ $t('page.home.Welcome.first') }}</h1>
+		<h1 data-aos="zoom-in" class="text-5xl md:text-7xl font-semibold text-white text-center">{{ $t('page.home.Welcome.first') }}</h1>
 		<div class="md:mt-4 flex flex-col items-center md:items-start md:flex-row md:space-x-4">
 			<p data-aos="zoom-in" data-aos-delay="300" class="text-5xl md:text-7xl font-semibold text-blue-400">{{ $t('page.home.Welcome.second') }}</p>
 			<p data-aos="fade-left" data-aos-delay="700" class="text-5xl md:text-7xl w-fit font-semibold text-blue-400 px-2 md:px-1 pb-4 border border-blue-500/50 relative decorateFirst">
