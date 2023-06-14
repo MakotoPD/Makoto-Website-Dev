4d655846d2e13589e8a3539e7d1fbf4e73b30f75
MakotoPL <patrydab4@gmail.com>
2023-06-14 10:58:09 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,7 +54,8 @@ export default defineNuxtConfig({
 		defaultLocale: 'en',
 		detectBrowserLanguage: {
 			useCookie: true,
-    		alwaysRedirect: true
+			cookieKey: 'i18n_redirected',
+    		redirectOn: 'root',
 		}
 	},
 	
