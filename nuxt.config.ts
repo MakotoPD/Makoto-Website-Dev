e29a4063bc015af4a002be711e5f16f948e2f265
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2023-09-20 11:09:34 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,11 +13,6 @@ export default defineNuxtConfig({
 					content: 'width=device-width, initial-scale=1'
 				},
 				{
-					hid: 'description',
-					name: 'description',
-					content: ''
-				},
-				{
 					name: 'format-detection',
 					content: 'telephone=no'
 				},
