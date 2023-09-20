4b3283e62f33c5349103ad08f0e2eb985e427af8
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2023-09-20 12:58:47 +0200

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="w-screen h-full main">
 		<header class="desktopmenu hidden w-full md:flex flex-col justify-center">
-			<div class="relative w-full flex justify-between p-2 border-b bg-gray-400/10 border-gray-500">
+			<div class="relative w-full flex justify-between p-2 border-b bg-gray-400/10 border-gray-500 backdrop-blur-sm">
 				<div>
 					<NuxtLink :to="localePath('/')">
 						<nuxt-img src="/logo.png" quality="20" alt="logo" class="h-8" />
